7bad5aa71d6168ab991ec51d60ebaa6dfe76f1d6
Amanda King <amapickell@gmail.com>
2020-05-12 22:56:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,13 +28,7 @@ class Student
     
     DB[:conn].execute(sql)
   end
-  
-  def self.create (name, grade)
-    student = self.new(name, grade)
-    student.save
-    student
-  end
-  
+
   def update
     sql = <<-SQL
       UPDATE students
@@ -57,6 +51,12 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id  = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+  end
+  
+  def self.create (name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
   end
 
 end
